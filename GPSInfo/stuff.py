60a13afe7f147a36cf86a22d100c19777b483022
Johnny Graber <jg@jgraber.ch>
2021-11-11 22:59:00 +0100

    add filehandling

--- GPSInfo/stuff.py
+++ GPSInfo/stuff.py
@@ -1,8 +1,16 @@
 import click
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
+# @click.command()
+# @click.argument('name')
+# def hello(name):
+#     click.echo(f'Hello {name}')
+
+@click.command(context_settings={"ignore_unknown_options": True})
+@click.argument('files', nargs=-1, type=click.Path(exists=True, file_okay=True, dir_okay=True, resolve_path=True))
+def hello(files):
+    """Print all FILES file names."""
+    for filename in files:
+        click.echo(filename)
 
 if __name__ == '__main__':
     hello()
