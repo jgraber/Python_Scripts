67dfe628a9bb6f55b53da8439f6265887f3b7bf5
Johnny Graber <jg@jgraber.ch>
2021-12-12 23:19:16 +0100

    Update stuff.py

--- GPSInfo/stuff.py
+++ GPSInfo/stuff.py
@@ -1,10 +1,5 @@
 import click
 
-# @click.command()
-# @click.argument('name')
-# def hello(name):
-#     click.echo(f'Hello {name}')
-
 @click.command(context_settings={"ignore_unknown_options": True})
 @click.argument('files', nargs=-1, type=click.Path(exists=True, file_okay=True, dir_okay=True, resolve_path=True))
 def hello(files):
