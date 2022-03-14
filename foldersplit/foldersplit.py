5bcbe004d58d433217d07af64487c382e287bee2
Johnny Graber <jg@jgraber.ch>
2022-03-14 22:55:00 +0100

    add method with folder argument

--- foldersplit/foldersplit.py
+++ foldersplit/foldersplit.py
@@ -7,5 +7,10 @@ def hello(count, name):
     for x in range(count):
         click.echo(f"Hello {name}!")
 
+@click.command()
+@click.argument('folder', type=click.Path(exists=True))
+def split(folder):
+    print(f"work with folder: {folder}")
+
 if __name__ == '__main__':
-    hello()
+    split()
