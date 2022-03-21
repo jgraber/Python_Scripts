1cbe4f803379a900c064b331f7580692723b0807
Johnny Graber <jg@jgraber.ch>
2022-03-21 18:55:27 +0100

    remove unused method hello

--- foldersplit/foldersplit.py
+++ foldersplit/foldersplit.py
@@ -1,13 +1,6 @@
 import click
 
 @click.command()
-@click.option('--count', default=1, help='number of greetings')
-@click.argument('name')
-def hello(count, name):
-    for x in range(count):
-        click.echo(f"Hello {name}!")
-
-@click.command()
 @click.option('--split-by',
               type=click.Choice(['day', 'month'], 
               case_sensitive=False),
