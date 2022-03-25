c81ca96fb2f6005e8ac954659d4ccfbe29f5c398
Johnny Graber <jg@jgraber.ch>
2022-03-25 21:30:05 +0100

    implement splitting of folder

--- foldersplit/foldersplit.py
+++ foldersplit/foldersplit.py
@@ -1,13 +1,40 @@
 import click
 import os
+import shutil
+from datetime import datetime
+from pathlib import Path
 
 def split_folder(folder, split_by):
     click.echo(f"work with folder: {folder} - split by {split_by}")
 
-    files = os.listdir(folder)
-    for file in files:
-        print(file)
-        # https://stackoverflow.com/questions/11348953/how-can-i-set-the-last-modified-time-of-a-file-from-python
+    move = []
+    with os.scandir(folder) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file():
+                stat = entry.stat()
+                dt_object = datetime.fromtimestamp(stat.st_ctime)
+                subfolder = _get_subfolder(dt_object, split_by)
+                path = Path(folder, subfolder, entry.name)
+                move.append((entry.path, str(path)))
+
+    for old, new in move:
+        dir_name, _ = os.path.split(new)
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+
+        print(f"{old} -> {new}")
+        shutil.move(old, new)
+        
+    # files = os.listdir(folder)
+    # for file in files:
+    #     print(file)
+    #     # https://stackoverflow.com/questions/11348953/how-can-i-set-the-last-modified-time-of-a-file-from-python
+
+def _get_subfolder(create_date, split_by):
+    if split_by == 'month':
+        return "_" + create_date.strftime('%Y-%m')
+
+    return "_" + create_date.strftime('%Y-%m-%d')
 
 @click.command()
 @click.option('--split-by',
--- foldersplit/test_foldersplit.py
+++ foldersplit/test_foldersplit.py
@@ -1,21 +1,43 @@
 import filedate
 from pathlib import Path
+import os
+import foldersplit
+import datetime
+from glob import glob
+import shutil
 
 def test_create_files():
-    a = 'test_data/aa.txt'
-    Path(a).write_text("")
-    a_file = filedate.File(a)
-    a_file.set(
-        created = "2022.01.01 13:00:00",
-        modified = "2022.01.01 14:00:00",
-        accessed = "2022.01.01 15:00:00"
-    )
+    os.makedirs('test_data',exist_ok=True)
+    folders = glob('test_data/_*', recursive = True)
+    for folder in folders:
+        shutil.rmtree(folder)
+    # a = 'test_data/aa.txt'
+    # Path(a).write_text("")
+    # a_file = filedate.File(a)
+    # a_file.set(
+    #     created = "2022.01.01 13:00:00",
+    #     modified = "2022.01.01 14:00:00",
+    #     accessed = "2022.01.01 15:00:00"
+    # )
+    create_file_with_date('test_data/aa.txt', "2022.01.01 13:00:00")
+    create_file_with_date('test_data/bb.txt', "2022.01.02 13:00:00")
 
-    b = 'test_data/bb.txt'
-    Path(b).write_text("")
-    b_file = filedate.File(b)
-    b_file.set(
-        created = "2022.01.02 13:00:00",
-        modified = "2022.01.02 14:00:00",
-        accessed = "2022.01.02 15:00:00"
+
+def test_correct_folder_for_day():
+    file_date = datetime.date(2021, 3, 4)
+    name = foldersplit._get_subfolder(file_date, 'day')
+    assert name == '_2021-03-04'
+
+def test_correct_folder_for_month():
+    file_date = datetime.date(2021, 3, 4)
+    name = foldersplit._get_subfolder(file_date, 'month')
+    assert name == '_2021-03'
+
+def create_file_with_date(path, date):
+    Path(path).write_text("")
+    new_file = filedate.File(path)
+    new_file.set(
+        created = date,
+        modified = date,
+        accessed = date
     )
