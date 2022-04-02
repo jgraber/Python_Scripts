0f2944aa89bbe6e911e23c0b70da8fad418ec763
Johnny Graber <jg@jgraber.ch>
2022-04-02 21:05:23 +0200

    extract format_number function

--- fileaway/fileaway_tests.py
+++ fileaway/fileaway_tests.py
@@ -1,9 +1,11 @@
 def test_1_is_printed_as_001():
     index = 1
-    formated = (str(index).rjust(3, '0'))
-    assert formated == '001'
+    assert format_numer(index) == '001'
 
 def test_1000_is_printed_as_1000():
     index = 1000
-    formated = (str(index).rjust(3, '0'))
-    assert formated == '1000'
+    assert format_numer(index) == '1000'
+
+def format_numer(number):
+    # https://python-reference.readthedocs.io/en/latest/docs/str/rjust.html
+    return str(number).rjust(3, '0')
