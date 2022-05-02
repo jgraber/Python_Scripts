30721d2a36e4f1251c60942d831ad5c322c67f04
Johnny Graber <jg@jgraber.ch>
2022-05-02 23:30:00 +0200

    add test to show how folders should be kept

--- fileaway/fileaway_tests.py
+++ fileaway/fileaway_tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_1_is_printed_as_001():
     index = 1
     assert format_numer(index) == '001'
@@ -9,3 +11,12 @@ def test_1000_is_printed_as_1000():
 def format_numer(number):
     # https://python-reference.readthedocs.io/en/latest/docs/str/rjust.html
     return str(number).rjust(3, '0')
+
+@pytest.mark.parametrize("input, expected",
+                         [(['a/a.txt'], ['a_001.txt']),
+                          (['a/a.txt', 'a/b.txt'], ['a_001.txt','a_002.txt']),
+                          (['a/a.txt', 'b/b.txt'], ['a_001.txt','b_001.txt'])
+                         ])
+def test_multiple_inputs(input, expected):
+    assert expected == input
+    print(f"{input}: {expected}")
