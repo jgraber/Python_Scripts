5c732287a54b2f92d75c1b69d7002bc6478786ce
Johnny Graber <jg@jgraber.ch>
2022-09-15 22:55:25 +0200

    refactoring: introduce class

--- ssl_checker/check_ssl_certs.py
+++ ssl_checker/check_ssl_certs.py
@@ -1,15 +1,28 @@
 # from https://stackoverflow.com/questions/41620369/how-to-get-ssl-certificate-details-using-python
 import socket
 import ssl
-import datetime
+from datetime import datetime
 from rich.console import Console
 from rich.table import Table
+from typing import NamedTuple
+
 
 domains_url = [
-"jgraber.ch",
 "ImproveAndRepeat.com",
-"ImproveRepeat.com"
+"python.org",
+"python-err.org"
 ]
+
+
+class CertificateInfo(NamedTuple):
+    domain: str
+    expiry_date: datetime
+    expires_in: int
+    issuer: str
+    
+    def expires_on(self):
+        return self.expiry_date.strftime('%Y-%m-%d')
+ 
 
 def ssl_expiry_datetime(hostname):
     ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
@@ -20,22 +33,31 @@ def ssl_expiry_datetime(hostname):
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
         server_hostname=hostname,
-    )
-    # 5 second timeout
+    )    
     conn.settimeout(5.0)
-
-    conn.connect((hostname, 443))
-    ssl_info = conn.getpeercert()
-    # https://stackoverflow.com/a/30863209/532064
-    issuer = dict(x[0] for x in ssl_info['issuer'])
-    issued_by = issuer['organizationName']
-    # print(ssl_info)
-    # Python datetime object
-    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_dateformat), issued_by
-
-if __name__ == "__main__":
-    # see https://improveandrepeat.com/2022/07/python-friday-132-rich-tables-for-your-terminal-apps/
     
+    conn.connect((hostname, 443))
+    certificate = conn.getpeercert()
+    
+    # https://stackoverflow.com/a/30863209/532064
+    issuer = dict(x[0] for x in certificate['issuer'])
+    issued_by = issuer['organizationName']
+    expire_date = datetime.strptime(certificate['notAfter'], ssl_dateformat)
+    remaining = expire_date - datetime.now()
+
+    return CertificateInfo(hostname, expire_date, remaining.days, issued_by)
+
+
+# if __name__ == "__main__":
+#     for value in domains_url:
+#         try:
+#             cert_info = ssl_expiry_datetime(value)
+#             print(f"{cert_info.domain}, {cert_info.expires_on()}, {cert_info.expires_in}, {cert_info.issuer}")
+#         except Exception as e:
+#             print(f"{value} {e.strerror}")
+        
+        
+if __name__ == "__main__":
     console = Console()
 
     table = Table(show_header=True, header_style="bold")
@@ -45,14 +67,13 @@ if __name__ == "__main__":
     table.add_column("Issuer")
     
     for value in domains_url:
-        now = datetime.datetime.now()
         try:
-            expire, issuer = ssl_expiry_datetime(value)
-            diff = expire - now
-            table.add_row(value, str(expire.strftime('%Y-%m-%d')), str(diff.days), issuer)
-            # print (f"Domain name: {value} \t\t Expiry Date: {} \t Expiry Day: {} \t Issuer: {}")
+            cert_info = ssl_expiry_datetime(value)
+            table.add_row(cert_info.domain, 
+                          str(cert_info.expires_on()), 
+                          str(cert_info.expires_in), 
+                          cert_info.issuer)
         except Exception as e:
-            # print(dir(e))
             table.add_row(value,"","",e.strerror)
     
     console.print(table)
